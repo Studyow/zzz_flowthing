e3fe2de9748f68a50278d88f114fd58b17b0a917
Studyow <letsgetdothese@gmail.com>
2020-12-11 18:05:58 +0800

    second flowthing commit

--- flowthing01/src/components/AddCookie.vue
+++ flowthing01/src/components/AddCookie.vue
@@ -1,25 +1,26 @@
 <template>
     <div class="add-cookie container">
-        <h2 class="center-align indigo-text">Add New Cookie Recipe</h2>
+        <h2 class="center-align indigo-text">Add new friends</h2>
         <form @submit.prevent="add_Cookie"> <!-- Created form for adding to new datafile, classes are from the DB-->
             
             <div class="field title">
                 <label for="title">Cookie Title:</label>
                 <input type="text" name="title" v-model="title">
             </div>
-
+            <!-- show ingredients -->
             <div v-for="(ing, index) in ingredients" :key="index">
                 <label for="ingredient">Ingredients:</label>
                 <input type="text" name="ingredient" v-model="ingredients[index]">
             </div>
-
+            <!-- Add ingredient -->
             <div class="field add-ingredient">
                 <label for="add-ingredient">Add an ingredient:</label>
                 <input type="text" name="add-ingredient" @keydown.tab.prevent="add_Ing" v-model="another">
+                <p>{{another}}</p>
             </div>
 
             <div class="field center-align">
-                <p v-if="feeback" class="red-text">{{ feedback }}</p>
+                <p v-if="feedback" class="red-text">{{ feedback }}</p>
                 <button class="btn pink">Add Cookie</button>
             </div>
         </form>
@@ -27,19 +28,43 @@
 </template>
 
 <script>
+import db from '@/firebase/init'
+import slugify from 'slugify'
+
+
 export default {
     name: 'AddCookie',
-    date(){
+    data(){
         return {
             title: null,
             another: null,
             ingredients: [],
-            feedback: null
+            feedback: null,
+            slug: null
         }
     },
     methods: {
         add_Cookie(){
-            console.log(this.title)
+            if (this.title){
+                this.feedback = null
+                // create a slug
+                this.slug = slugify(this.title, {
+                    replacement: '-',
+                    remove: /[$*_+~.()'"!-:@]/g,
+                    lower: true
+                })
+                db.collection('cookies').add({
+                    title: this.title,
+                    ingredients: this.ingredients,
+                    slug: this.slug
+                }).then(() => {
+                    this.$router.push({ name: 'Index' })
+                }).catch(err => {
+                    console.log(err)
+                })
+            } else {
+                this.feedback = 'you must enter a cookie title'
+            }
         },
         add_Ing(){
             if(this.another) {
